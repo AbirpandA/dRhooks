a8f1a76523f6dcb29a6580a7390e902e7d7a7ea2
AbirpandA <abirpanda335@gmail.com>
2026-01-29 21:57:11 +0530

    feat: initialized testing enviroment for components and created test for usedebounce hooks

---
+++ src/registry/new-york/hooks/use-debounce.test.ts
@@ -0,0 +1,34 @@
+import { act, renderHook } from "@testing-library/react";
+import { useDebounce } from "./use-debounce";
+import { vi, it, describe, beforeEach, afterEach, expect } from "vitest";
+
+describe("useDebounce", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should return the initial value immediately without delay", () => {
+    const { result } = renderHook(() => useDebounce("initial", 300));
+    expect(result.current).toBe("initial");
+  });
+  it("should update the debounce value after the specified delay", () => {
+    const { result, rerender } = renderHook(
+      ({ value, delay }) => useDebounce(value, delay),
+      {
+        initialProps: { value: "initial", delay: 300 },
+      }
+    );
+
+    expect(result.current).toBe("initial");
+
+    rerender({ value: "updated", delay: 300 });
+
+    act(() => {
+      vi.advanceTimersByTime(300);
+    });
+    expect(result.current).toBe("updated");
+  });
+});
